4c77ebe9832470d1c47332ed6d5d439c6a6c3243
kyomic <kyomic@163.com>
2025-03-11 00:15:09 +0800

    feat: 修复认证

--- session/login.py
+++ session/login.py
@@ -11,13 +11,17 @@ from io import BytesIO
 from urllib.parse import urlencode
 class Loginable(scrapy.Spider):
     session = {}
+    # 是否缓存session
     cache_session = False
+    cookie_parts = []
     def __init__(self):
         print('init Loginable')
+        self.logout()
         #input('请输入用户名:')
 
     def generate_useragent(self):
         return 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    
     
     def get_session_file_name(self):
         return '0067.org.session'
@@ -26,7 +30,62 @@ class Loginable(scrapy.Spider):
     def login(self, username, password):
         return True
     
-    def init_session(self):
+    
+    def parse_cookie_str( self, str ):
+        cookies = str.split(';')
+        # 提取每个 cookie 的 key=value 部分
+        for cookie in cookies:
+            # 按分号分割，取第一个部分即 key=value
+            cookie = re.sub(r'path=/,', '', cookie)
+            key_value = cookie.strip()
+            
+            print("value", key_value)
+            # 检查是否包含 '='，如果不包含则跳过
+            if '=' not in key_value:
+                continue
+            # 按等号分割，取第一个部分即 key
+            key = key_value.split('=')[0]
+            value = key_value.split('=')[1]
+            # 检查 key 是否为 'expires' 或 'Max-Age'，如果是则跳过
+            if key in ['expires', 'Max-Age', 'path']:
+                continue
+            # 查找 cookie_parts 中是否已经存在该 key
+            found = False
+            for c in self.cookie_parts:
+                if c.get('name') == key:
+                    # 如果存在，更新该 key 对应的值
+                    c['value'] = value
+                    found = True
+                    break
+            # 如果不存在，添加新的 key-value 对
+            if not found:
+                self.cookie_parts.append({'name': key, 'value':value})
+            # 否则将 key=value 添加到结果列表
+            print("COOKIE", self.cookie_parts)
+        # 重新组合成字符串
+        return self.cookie_parts
+    
+    def update_cookie(self, response):
+        str = response.headers['Set-Cookie']
+        self.parse_cookie_str(str)
+    
+    def get_cookie(self):
+        """
+        获取拼接后的 cookie 字符串。
+        该方法会检查 self.cookie_parts 是否为 None，如果是则将其初始化为空列表，
+        然后将 self.cookie_parts 中的每个字典元素的 'value' 字段值提取出来，
+        并使用分号 ';' 连接成一个字符串返回。
+    
+        :return: 拼接后的 cookie 字符串
+        """
+        # 检查 self.cookie_parts 是否为 None，如果是则初始化为空列表
+        if self.cookie_parts is None:
+            self.cookie_parts = []
+        
+        print('COOKIE=',self.cookie_parts)
+        return  ';'.join([f"{item['name']}={item['value']}" for item in self.cookie_parts])
+        
+    def load_session( self ):
         filename = self.get_session_file_name()
         if os.path.exists(filename) and self.cache_session:
             print('session file exists')
@@ -34,47 +93,68 @@ class Loginable(scrapy.Spider):
             try:
                 with open(filename, 'r') as file:
                     self.session = json.load(file)
+                
+                cookie = self.session['cookie']
+                
+                if cookie is not None:
+                    self.parse_cookie_str(cookie)
+                
                 print(f"Session 数据已成功从 {filename} 读取")
             except Exception as e:
                 print(f"读取文件时出错: {e}")
-            
+                
+    def init_session(self):
+        filename = self.get_session_file_name()
+        if os.path.exists(filename) and self.cache_session:
+            self.load_session()
         else:
             print('session file not exists')
-            response = requests.get(
-                url='https://0067.org/user-login.html',
-                headers={
-                    'User-Agent': self.generate_useragent(),
-                }
-            )
+            print('获取Token')
+            try:
+                response = requests.get(
+                    url='https://0067.org/user-login.html',
+                    headers={
+                        'User-Agent': self.generate_useragent(),
+                        'Cookie': self.get_cookie()
+                    }
+                )
+                response.raise_for_status()
+                self.update_cookie(response)
+                print('获取Token成功')
+
+            except Exception as e:
+                print(f"获取Token时出错: {e}")
+                return
+            try:
+                response_qrcode = requests.get(
+                    url='https://0067.org/index.php?s=Vcode-Index',
+                    headers={
+                        'User-Agent': self.generate_useragent(),
+                        #'Cookie': self.get_cookie()
+                    } 
+                )
+                response_qrcode.raise_for_status()
+                self.update_cookie(response_qrcode)
+                image = Image.open(BytesIO(response_qrcode.content))
+                download_path = "downloaded_image.jpg"
+                image.save(download_path)
+                print(f"图片已下载到 {download_path}")
+            except Exception as e:
+                return
+            
+           
+                
+            
             if not isinstance(self.session, dict):
                 self.session = {}
             
             str = "_gj=cRQeXUVDXElWVjMAY3YZQBZaHToTRnZFTg; expires=Thu, 06-Mar-2025 05:22:03 GMT; Max-Age=86400; path=/, _dq=cRQeXUVDXElWVjMAY3YZQhZdTzoTRnMSHQ; expires=Thu, 06-Mar-2025 05:22:03 GMT; Max-Age=86400; path=/"
             # 按逗号分割字符串，处理多个 cookie
-            str = response.headers['Set-Cookie']
-            cookies = str.split(';')
-            # 提取每个 cookie 的 key=value 部分
-            cookie_parts = []
-            for cookie in cookies:
-                # 按分号分割，取第一个部分即 key=value
-                cookie = re.sub(r'path=/,', '', cookie)
-                key_value = cookie.strip()
-                
-                print("value", key_value)
-                # 检查是否包含 '='，如果不包含则跳过
-                if '=' not in key_value:
-                    continue
-                # 按等号分割，取第一个部分即 key
-                key = key_value.split('=')[0]
-                # 检查 key 是否为 'expires' 或 'Max-Age'，如果是则跳过
-                if key in ['expires', 'Max-Age', 'path']:
-                    continue
-                # 否则将 key=value 添加到结果列表
-                cookie_parts.append(key_value)
-            # 重新组合成字符串
-            cleaned_cookies = '; '.join(cookie_parts)
-            self.session['cookie'] = cleaned_cookies
-
+            
+           
+            
+           
+            self.session['cookie'] =  self.get_cookie()
             filename = self.get_session_file_name()
             try:
                 with open(filename, 'w') as file:
@@ -99,7 +179,7 @@ class Loginable(scrapy.Spider):
                 'user_email':'riacn@qq.com',
                 'user_pwd':'1qaz1qaz',
                 'user_vcode':{
-                    'type':'qrcode',
+                    'type':'input',
                     'url': 'https://0067.org/index.php?s=Vcode-Index',
                     'name':'验证码',
                 },
@@ -107,6 +187,15 @@ class Loginable(scrapy.Spider):
             },
         }
 
+    def logout(self):
+        self.load_session()
+        requests.get(
+            url='https://0067.org/user-logout.html',
+            headers={
+                'User-Agent': self.generate_useragent(),
+                'Cookie': self.get_cookie()
+            }
+        )
     def check_login(self):
         session = self.init_session()
         
@@ -133,12 +222,15 @@ class Loginable(scrapy.Spider):
                         if sub_url is None:
                             raise Exception("sub_url is None, please check config_login")
                         
+                        php_session_id_cookie = [cookie for cookie in self.session_cookies if 'PHPSESSID' in cookie]
+                        print('php session', php_session_id_cookie)
                         response = requests.get(sub_url, headers={
                             'User-Agent': self.generate_useragent(),
-                            'Cookie': session['cookie']
+                            #'Cookie': '; '.join(php_session_id_cookie)
                         })
-                        print("会话cookie:", session)
+                        print("会话cookie:", session, response.headers)
                         response.raise_for_status()
+                        
                         # 打开图片
                         image = Image.open(BytesIO(response.content))
                         download_path = "downloaded_image.jpg"
@@ -149,17 +241,28 @@ class Loginable(scrapy.Spider):
 
             headers = {
                 'User-Agent': self.generate_useragent(),
-                'Cookie': session['cookie']
+                'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
+                #'Origin':'https://0067.org',
+                #'Referer':'https://0067.org/user-login.html',
+                'Cookie': self.get_cookie()
             }
+            print('请求:', config['url'])
+            print('请求头:', headers)
             response = requests.post(
                 url=config['url'],
                 headers=headers,
                 data=urlencode(config['data']).encode('utf-8'), 
             )
             json = response.json()
+            respnse_header = response.headers
             print('response',json)
+            print('响应头:',respnse_header)
             if( response.status_code !=200):
                 print('登录失败', json.info)
+                return
+            if json and json['data'] == 0:
+                print('登录失败', json['info'])
+                self.logout()
                 return
         except Exception as e:
             print(f"执行 check_login 时出错: {e}")
@@ -183,14 +286,14 @@ class Abc():
         super().__init__()  # 调用 Loginable 的 __init
         
         config = {
-            'url': 'https://bt66.org/user-loginpost.html',
+            'url': 'https://0067.org/user-login.html',
             'method': 'POST',
             'data': {
                 'user_email':'riacn@qq.com',
                 'user_pwd':'1qaz1qaz',
                 'user_vcode':{
                     'type':'qrcode',
-                    'url': 'https://bt66.org/index.php?s=Vcode-Index',
+                    'url': 'https://0067.org/index.php?s=Vcode-Index',
                     'name':'验证码',
                 },
                 'user_remember':1
